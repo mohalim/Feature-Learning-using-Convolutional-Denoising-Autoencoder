76909094ded166d9e6f32ad5f55214eab2ca5d8b
mohalim <mohalimnoor@gmail.com>
2020-01-09 22:04:06 +0800

    Update and rename activity_recognition_conv_ae_fc_3ch_tf.py to classifier.py

--- classifier.py
+++ classifier.py
@@ -17,53 +17,27 @@ ae_fc_model = 'Models/cdae_fc_elu_filters_' + str(latent_filter) + '_fold_' + st
 is_train = 1
 is_test = 1
 is_load = 0
-is_transition = 0
 
 y_test = np.copy(test_label)
 
-#if is_transition:
-#    y_train[y_train == 4] = 2
-#    y_train[y_train == 5] = 2
-#    y_train[y_train == 7] = 2
-#    y_test[y_test == 4] = 2
-#    y_test[y_test == 5] = 2
-#    y_test[y_test == 7] = 2
 
 learning_rate = 0.5 # fold1: 0.7, fold2: 0.5, fold3: 0.7
-display_step = 40
-decay_step = 20 # fold1: 20, fold2:  , fold3: 20
-decay_rate = 0.97   # the best so far 0.95
+display_step = 20
 min_delta = 0.00001
 patience = 100
-epoch = 1500    # fold1: 3500, fold2: 3500, fold3: 3500
-if fold == 1:
-    h1 = 40
-    h2 = 20
-if fold == 2:
-    h1 = 40
-    h2 = 20
-if fold == 3:
-    h1 = 40
-    h2 = 20
+epoch = 1000    # fold1: 3500, fold2: 3500, fold3: 3500
+
+h1 = 40
+h2 = 20
 
 tf.reset_default_graph()
-#conv_ae_graph = tf.Graph()
-#with conv_ae_graph.as_default():
+
 saver = tf.train.import_meta_graph(ae_model+'model.ckpt.meta')
 sess = tf.Session()
 saver.restore(sess, ae_model+'model.ckpt')
 ae_graph = tf.get_default_graph()
-#operations = ae_graph.get_operations()
-#for op in operations:
-#    if 'encoder/latent' in op.name:
-#        print(op.name)
 
-sigmoid = tf.nn.sigmoid
-tanh = tf.nn.tanh
-relu = tf.nn.relu
-lrelu = tf.nn.leaky_relu
-elu = tf.nn.elu
-act_fn = elu
+act_fn = tf.nn.elu
 
 X_net = ae_graph.get_tensor_by_name('X_noise:0')
 y = tf.placeholder(tf.float32, shape=(None, batch_y.shape[1]), name='y')
@@ -79,10 +53,6 @@ with tf.variable_scope('fully_connected', reuse=None):
                           kernel_initializer=tf.contrib.layers.xavier_initializer(),
                           use_bias=True, bias_initializer=tf.zeros_initializer())
     fc2 = tf.layers.dropout(fc2)
-#    fc3 = tf.layers.dense(fc2, 50, activation=tf.nn.elu,
-#                          kernel_initializer=tf.contrib.layers.xavier_initializer(),
-#                          use_bias=True, bias_initializer=tf.zeros_initializer())
-#    fc3 = tf.layers.dropout(fc3)
     logits = tf.layers.dense(fc2, batch_y.shape[1], activation=None,
                           kernel_initializer=tf.contrib.layers.xavier_initializer(),
                           use_bias=True, bias_initializer=tf.zeros_initializer())
@@ -97,11 +67,9 @@ with tf.name_scope('loss'):
     
 with tf.name_scope('train'):
     train_weights = tf.trainable_variables()
-#        enc_weights = [w for w in train_weights if 'encoder' in w.name]
     fc_weights = [w for w in train_weights if 'fully_connected' in w.name]
     
     optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
-#    solver = optimizer.minimize(loss, var_list=fc_weights)
     solver = optimizer.minimize(loss, var_list=fc_weights)
 
 pred = tf.argmax(softmax, 1)
@@ -126,8 +94,6 @@ if is_train:
                 avg_acc = 0
                 n_batches = len(x_batches)
                 for b in range(0,n_batches):
-#                    batch_x = tf.slice(x_train_noise, [idx, 0, 0], [batch_size, -1, -1]).eval()
-#                    batch_y = tf.slice(y_train, [idx, 0], [batch_size, -1]).eval()
                     batch_x = x_batches[b]
                     _, loss_val, acc_val = sess.run([solver, loss, accuracy], 
                                            feed_dict={X_net: batch_x, 
@@ -158,7 +124,6 @@ if is_train:
                     print('epoch:', '%04d'%(e),
                           'cost:', '{:.6f}'.format(avg_loss),
                           'acc:', '{:.6f}'.format(avg_acc))
-                    print('early stopping...')
                     break
     
 if is_test:
@@ -174,5 +139,3 @@ if is_test:
     print(acc_score)
     print(conf_mat)
     print(cls_report)
-
-#y_compare = np.hstack((y_test.reshape(-1,1),y_pred.reshape(-1,1)))
