5d296f50f95c3a1190f58333f61eef014a34e2c2
mohalim <mohalimnoor@gmail.com>
2020-01-09 21:59:44 +0800

    Update and rename activity_recognition_conv_ae_3ch_tf.py to feature_learning.py

--- feature_learning.py
+++ feature_learning.py
@@ -17,27 +17,9 @@ import os
 import train_data_raw_pa_hapt_100 as tr
 import test_data_raw_pa_hapt_100 as ts
 
-def normalize(data, x_range, feat_range=[-1,1]):
-    r = 0
-    c = 0
-    data_norm = np.zeros((data.shape[0], data.shape[1]))
-    for r in range(0,data.shape[0]):
-        for c in range(0,data.shape[1]):
-            data_std = (data[r,c] - x_range[0]) / (x_range[1] - x_range[0])
-            data_norm[r,c] = data_std * (feat_range[1] - feat_range[0]) + feat_range[0]
-    return data_norm
 
-def plot(size, data):
-    fig = plt.figure()
-#    print(data.shape)
-    grid = gs.GridSpec(size[0],size[1])
-    for i in range(0,len(data)):
-        plt.subplot(grid[i])
-        plt.plot(data[i,:])
-
-fold = 2
+fold = 1
 use_bias = 1
-regularizer = 'none'
 
 is_get_data = 0 # get train data and test data
 is_add_noise = 0
@@ -52,27 +34,12 @@ if is_get_data:
     test_data, test_label = ts.get_test_data(fold)
 smpl, n_length, n_channels = train_data.shape[0],train_data.shape[1],train_data.shape[2]
 
-feat_range=[0,1]
-x_train = np.empty((train_data.shape[0],train_data.shape[1],0))
-for d in range(0,3):
-    tmp = normalize(train_data[:,:,d], [-10, 10], feat_range=feat_range)
-    tmp = tmp.reshape(tmp.shape[0],tmp.shape[1],1)
-    x_train = np.dstack((x_train,tmp))
-
-x_test = np.empty((test_data.shape[0],test_data.shape[1],0))
-for d in range(0,3):
-    tmp = normalize(test_data[:,:,d], [-10, 10], feat_range=feat_range)
-    tmp = tmp.reshape(tmp.shape[0],tmp.shape[1],1)
-    x_test = np.dstack((x_test,tmp))
-
-
 noise_thres = 0.05
 if is_add_noise:
     x_train_noise = np.zeros(shape=(smpl, n_length, n_channels))
     for c in range(0,3):
         noise = np.random.uniform(low=-noise_thres, high=noise_thres, size=(smpl, n_length))
         x_train_noise[:,:,c] = x_train[:,:,c] + noise
-#    plt.figure();plt.subplot(211);plt.plot(x_train[25,:,1]);plt.subplot(212);plt.plot(x_train_noise[25,:,1])
 
 if is_create_batches:
     n_batch = 80
@@ -125,31 +92,20 @@ if is_create_batches:
         batch_y[st:ed] = c
         st = ed; ed = ed+n_smpl
     batch_y = tf.keras.utils.to_categorical(batch_y)
-    
     del batch, batch_noise, tmp, dat, data, data_noise, n_batch, n_smpl, st, ed
 
-keep_prob = 0.5
-learning_rate = 0.05
-decay_step = 20
-decay_rate = 0.97
-epoch = 500     # fold1: 1500   , fold2: 2000    , fold3: 2000
-reg_scale = 0.001
+
+epoch = 1000
 display_step = 20
-min_delta = 0.000001 # elu 0.00001 lrelu 0.000001
+min_delta = 0.000001 # elu 0.00001
 patience = 100
 min_loss_th = 0.001
 latent_filter = 4
 
 xavier = tf.contrib.layers.xavier_initializer()
-sigmoid = tf.nn.sigmoid
-tanh = tf.nn.tanh
-relu = tf.nn.relu
-lrelu = tf.nn.leaky_relu
-elu = tf.nn.elu
-act_fn = elu
+act_fn = tf.nn.elu
 
 dir_model = 'Models/cdae_elu_filters_' + str(latent_filter) + '_fold_' + str(fold) + '/'   # cnn2:maxpool cnn3:avgpool
-
 
 tf.reset_default_graph()
 X_noise = tf.placeholder(tf.float32, shape=(None, n_length, n_channels), name='X_noise')
@@ -180,7 +136,6 @@ with tf.name_scope('network'):
                                   kernel_initializer=xavier,
                                   data_format='channels_last', activation=act_fn, name='conv3')
             conv3 = tf.layers.dropout(conv3)
-#            conv3 = tf.pad(conv3, tf.constant([[0,0], [0,1], [0,0]]))
             pool3 = tf.layers.max_pooling1d(conv3, 2, 2)
             
             conv4 = tf.layers.conv1d(pool3, filters=40,
@@ -228,8 +183,6 @@ with tf.name_scope('network'):
                                   data_format='channels_last', activation=act_fn, name='conv2')
             conv2 = tf.layers.dropout(conv2)
             up2 = tf.keras.layers.UpSampling1D(2)(conv2)
-#            up2 = tf.pad(up2, tf.constant([[0,0], [0,1], [0,0]]))
-#            up2 = tf.slice(up2, [0,0,0], [-1,25,-1])
             
             conv3 = tf.layers.conv1d(up2, filters=20, # 12
                                   kernel_size=7, padding='SAME',
@@ -246,7 +199,6 @@ with tf.name_scope('network'):
                                   kernel_initializer=xavier,
                                   data_format='channels_last', activation=act_fn, name='conv4')
             conv4 = tf.layers.dropout(conv4)
-#            conv4 = tf.pad(conv4, tf.constant([[0,0], [0,1], [0,0]]))
             up4 = tf.keras.layers.UpSampling1D(2)(conv4)
             
             logits = tf.layers.conv1d(up4, filters=3,   # 100
@@ -334,23 +286,16 @@ with tf.name_scope('loss'):
                                                           logits=d_logits_fake), name='d_loss_fake')
     d_loss = d_loss_real + d_loss_fake
     ae_loss = tf.reduce_mean(tf.square(X - logits))
-#    ae_loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(labels=X, logits=logits, name='xentropy'))
     lambd = 1e-3
     network_loss = ae_loss + lambd*d_loss
     
 
 with tf.name_scope('train'):
-    global_step = tf.Variable(0, trainable=False, name='global_step')
-    decayed_rate = tf.train.exponential_decay(learning_rate, global_step, decay_step, decay_rate, True)
-#    optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
-#    optimizer = tf.train.AdadeltaOptimizer(learning_rate=decayed_rate) # fold 1 & 2: 0.01, fold 3: 0.05
     optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
     solver = optimizer.minimize(network_loss, var_list=[enc_weights, dec_weights, disc_weights])
 
 
 if is_train:
-#    n_splits = 5
-#    kf = KFold(n_splits=n_splits)
     saver = tf.train.Saver() 
     min_loss = 0
     with tf.name_scope('eval'):
@@ -389,8 +334,6 @@ if is_train:
                 hist_d_loss.append(avg_d_loss)
                 hist_net_loss.append(avg_net_loss)
                 if e % display_step == 0:
-#                    loss_val = sess.run([loss], feed_dict={X:x_train})
-#                    loss_val = loss_val[-1]
                     print('epoch:', '%04d'%(e),
                           'cost_net:', '{:.6f}'.format(avg_net_loss),
                           'cost_ae:', '{:.6f}'.format(avg_ae_loss),
@@ -411,63 +354,5 @@ if is_train:
                           'cost_net:', '{:.6f}'.format(avg_net_loss),
                           'cost_ae:', '{:.6f}'.format(avg_ae_loss),
                           'cost_d:', '{:.6f}'.format(avg_d_loss))
-                    print('early stopping...')
                     break
-                
-                if avg_ae_loss < min_loss_th:
-                    print('epoch:', '%04d'%(e),
-                          'cost_net:', '{:.6f}'.format(avg_net_loss),
-                          'cost_ae:', '{:.6f}'.format(avg_ae_loss),
-                          'cost_d:', '{:.6f}'.format(avg_d_loss))
-                    print('threshold exceed...')
-                    break
-
-
-
-if is_analyse:
-    # fold1: 32, 50, 63, 195, 251
-    # fold2: 178, 185, 195, 300
-    # analysis fold1: 96(stsi->std), 434(stsi->walk), 188,374(sist->walk)
-    # fold1: 25(st),
-    n_smpl = 178
-    n_chn = 1
-    saver = tf.train.Saver()
-    with tf.Session() as sess:
-        saver.restore(sess, dir_model+'model.ckpt')
-        logit_out, latent_out = sess.run([logits, h], feed_dict={X_noise:x_test[n_smpl,:,:].reshape(1,n_length,n_channels)})
-        signal = logit_out[0,:,n_chn]
-        feats = latent_out[0,:]
-        plt.figure()
-#        plt.subplot(211)
-        plt.plot(signal, label='Reconstructed Signal')
-        plt.plot(x_test[n_smpl,:,n_chn], 'r', label='Input Signal')
-        plt.grid()
-        plt.xticks(fontsize=20)
-        plt.yticks(fontsize=20)
-        plt.ylabel('acceleration, $m/2^2$', fontsize=20)
-#        plt.legend(fontsize='20')
-#        plt.subplot(212);plt.plot(feats);plt.grid()
-#        plt.plot(signal)
-#        plt.plot(feats)
-
-#with tf.Session() as sess:
-#    if is_load:
-#        saver.restore(sess, dir_model+'model.ckpt')
-#        saver.save(sess, dir_model+'model.ckpt')
-
-#if is_get_weights:
-#    weights = tf.get_default_graph()
-
-#n = 50
-#c = 1
-#plt.subplot(211);plt.plot(test_data[n,:,c]);plt.grid()
-#plt.subplot(212);plt.plot(x_test[n,:,c]);plt.grid()
-#
-#n = 940
-#c = 0
-#plt.subplot(211);plt.plot(train_data[n,:,c]);plt.grid()
-#plt.subplot(212);plt.plot(x_train[n,:,c]);plt.grid()
-#
-#plt.subplot(211);plt.plot(x_train[n,:,1]);plt.grid()
-#plt.subplot(212);plt.plot(x_train_noise[n,:,1]);plt.grid()
 
